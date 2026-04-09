6b32e771ae17ebd45e7a3df63e565229ae1905ed
kairusds <32636532+kairusds@users.noreply.github.com>
2026-04-10 00:30:39 +0800

    Bump SyncFav version

--- plugins/SyncFav/build.gradle.kts
+++ plugins/SyncFav/build.gradle.kts
@@ -1,11 +1,14 @@
-version = "1.0.1"
+version = "1.0.2"
 description = "Backports the sync feature for favorite emojis from the modern Discord clients."
 
 aliucord {
     changelog.set(
         """
+        # 1.0.2
+        * Fix data loss when sending packets
+
         # 1.0.1
-        Use safer HTTP calls
+        * Use safer HTTP calls
         
         # 1.0.0
         * Initial release
